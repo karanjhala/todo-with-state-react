3038c5e9d2154da9242e07165e3a696c7ad23ba0
karanjhala <38532308+karanjhala@users.noreply.github.com>
2021-05-24 07:49:34 +0800

    make app interactive using state

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import Form from "./components/Form";
+import FilterButton from "./components/FilterButton";
+import Todo from "./components/Todo";
+import { nanoid } from "nanoid";
+
+
+function App(props) {
+  const [tasks, setTasks] = useState(props.tasks);
+  
+  function addTask(name) {
+    const newTask = { id: "id", name: name, completed: false };
+    setTasks([...tasks, newTask]);
+  }
+  
+  const taskList = tasks.map(task => (
+    <Todo
+        id={task.id}
+        name={task.name}
+        completed={task.completed}
+        key={task.id}
+      />
+    )
+  );
+  return (
+    <div className="todoapp stack-large">
+      <Form addTask={addTask} />
+      <div className="filters btn-group stack-exception">
+        <FilterButton />
+        <FilterButton />
+        <FilterButton />
+      </div>
+      <h2 id="list-heading">3 tasks remaining</h2>
+      <ul
+        className="todo-list stack-large stack-exception"
+        aria-labelledby="list-heading"
+      >
+        {taskList}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
