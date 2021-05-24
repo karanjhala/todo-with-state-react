24caffedc3d2527859a2a9c413f5526634deaf0b
karanjhala <38532308+karanjhala@users.noreply.github.com>
2021-05-24 08:28:05 +0800

    minor changes

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import './index.css';
 import App from './App';
 
 const DATA = [
-  { id: "todo-0", name: "Eat", completed: true },
-  { id: "todo-1", name: "Sleep", completed: false },
-  { id: "todo-2", name: "Repeat", completed: false }
+  { id: "todo-0", name: "Starter", completed: true },
+  { id: "todo-1", name: "Demo", completed: false },
+  { id: "todo-2", name: "Tasks", completed: false }
 ];
 
 ReactDOM.render(<App tasks={DATA} />, document.getElementById("root"));
